76788cfcaf59824890cf820e5865c03176d538a3
shivr2035 <shivr2035@gmail.com>
2018-04-23 00:45:39 -0400

    Rework code to live in classes for cleaner implementation

--- manage.py
+++ manage.py
@@ -1,141 +1,155 @@
-import os, configparser, subprocess, shutil, sys
+import os, configparser, subprocess, shutil, sys, math
 from jinja2 import Environment, FileSystemLoader, Template
 
-def read_config():
-  config = configparser.ConfigParser()
-  config.read('swarm.ini')
-  return config
+DRY_RUN = True
 
-def current_dir():
-  return os.path.dirname(os.path.abspath(__file__))
+class Config:
 
-def scripts_dir():
-  return os.path.join(
-    current_dir(), 
-    'scripts')
+  def __init__(self):
+    self.config = configparser.ConfigParser()
+    self.config.read('swarm.ini')
 
-def playbooks_dir():
-  return os.path.join(
-    current_dir(), 
-    'scripts')
-
-def cfg_ansible_dir():
-  return os.path.join(
-    current_dir(), 
-    'cfg-ansible')
-
-def testbed_dir(config):
-  return os.path.join(
-    current_dir(), 
-    'bld', 
-    config['Default']['BuildDirectory'], 
-    'testbed')
-
-def keyfiles(config):
-  pubfile = os.path.join(
-    current_dir(), 
-    config['Default']['PublicKeyFile'])
-  privfile = os.path.join(
-    current_dir(), 
-    config['Default']['PrivateKeyFile'])
-  return (pubfile, privfile)
-
-def ensure_dir_exists(dirname):
-  if not os.path.exists(dirname):
-    os.makedirs(dirname)
-
-def compile_testbed():
-  config = read_config()
-  env = Environment(
-    loader=FileSystemLoader(current_dir()), 
-    trim_blocks=True)
-  vgfile_template = env.get_template('vagrantfiles/Vagrantfile.j2')
-  
-  vgfile_contents = vgfile_template.render(
-    TestbedSize=config['Testbed']['TestbedSize'],
-    IPMask24=config['Testbed']['IPMask24'],
-    IPMask8Offset=config['Testbed']['IPMask8Offset'])
-  
-  testbed_build_dir = testbed_dir(config)
-  vgfile_filename = os.path.join(testbed_build_dir, 'Vagrantfile')
-  vgfile_hostsfile = os.path.join(testbed_build_dir, 'hosts')
-
-  ensure_dir_exists(testbed_build_dir)
-  with open(vgfile_filename, 'w+') as vgfile_out:
-    vgfile_out.write(vgfile_contents)
-
-  with open(vgfile_hostsfile, 'w+') as vgfile_hosts:
-    testbed_size = int(config['Testbed']['TestbedSize'])
-    ipmask8offset = int(config['Testbed']['IPMask8Offset'])
+    self.current_dir = os.path.dirname(os.path.abspath(__file__))
     
-    for i in range(testbed_size):
-      ip_str = config['Testbed']['IPMask24'] + '.' + str(i + 1 + ipmask8offset)
-      vgfile_hosts.write(ip_str + '\n')
+    self.scripts_dir = os.path.join(self.current_dir, 'scripts')
+    self.playbooks_dir = os.path.join(self.current_dir, 'playbooks')       
+    self.ansible_cfg_dir = os.path.join(self.current_dir, 'ansible-cfg')
+    self.vagrant_dir = os.path.join(self.current_dir, 'vagrant')
+    self.bld_dir = os.path.join(self.current_dir, '.bld')
+
+    self.using_testbed = bool(self.config['Default']['UsingTestbed'])
+    self.build_dir = os.path.join(self.bld_dir, self.config['Default']['BuildDirectory'])
+    self.compose_dir = os.path.join(self.current_dir, self.config['Default']['ComposeDirectory'])
+    self.hosts_file = os.path.join(self.current_dir, self.config['Default']['HostsFile'])
+    self.public_key_file = os.path.join(self.current_dir, self.config['Default']['PublicKeyFile'])
+    self.private_key_file = os.path.join(self.current_dir, self.config['Default']['PrivateKeyFile'])
+
+    self.testbed_dir = os.path.join(self.build_dir, 'testbed')
+    self.testbed_size = int(self.config['Testbed']['TestbedSize'])
+    self.ip_mask_24 = self.config['Testbed']['IPMask24']
+    self.ip_mask_8_offset = int(self.config['Testbed']['IPMask8Offset'])
   
-  (pubfile, privfile) = keyfiles(config)
+  def print_config(self):
+    print("Current directory = " + self.current_dir)
+    print("Scripts directory = " + self.scripts_dir)
+    print("Playbooks directory = " + self.playbooks_dir)
+    print("Ansible config directory = " + self.ansible_cfg_dir)
+    print("Vagrant directory = " + self.vagrant_dir)
+    print(".bld directroy = " + self.bld_dir)
+    print("Using testbed? " + str(self.using_testbed))
+    print("Build directory = " + self.build_dir)
+    print("Compose directory = " + self.compose_dir)
+    print("Hosts file = " + self.hosts_file)
+    print("Public key file = " + self.public_key_file)
+    print("Private key file = " + self.private_key_file)
+    print("Testbed directory = " + self.testbed_dir)
+    print("Testbed size = " + str(self.testbed_size))
+    print("First 24 bits of testbed IPs = " + self.ip_mask_24)
+    print("Offset of last 8 bits of testbed IPs = " + self.ip_mask_8_offset)    
 
-  shutil.copyfile(pubfile, os.path.join(testbed_build_dir, 'server.pem.pub'))
-  shutil.copyfile(privfile, os.path.join(testbed_build_dir, 'server.pem'))
-  # shutil.copyfile(
-  #   os.path.join(scripts_dir(), 'new-ssh-keys.sh'),
-  #   os.path.join(testbed_build_dir, 'new-ssh-keys.sh'))
+class Testbed:
 
-def boot_up_testbed():
-  config = read_config()
-  testbed_build_dir = testbed_dir(config)
-  vgfile_filename = os.path.join(testbed_build_dir, 'Vagrantfile')
-  vgfile_hostsfile = os.path.join(testbed_build_dir, 'hosts')
+  def __init__(self):
+    self.config = Config()
+    if not os.path.exists(self.config.testbed_dir):
+      os.makedirs(self.config.testbed_dir)
+    
+    self.env = Environment(
+      loader=FileSystemLoader(self.config.current_dir), 
+      trim_blocks=True)
+    self.template = self.env.get_template('vagrant/Vagrantfile.j2')
+    self.template_context = {
+      'TestbedSize': self.config.testbed_size,
+      'IPMask24': self.config.ip_mask_24,
+      'IPMask8Offset': self.config.ip_mask_8_offset
+    }
 
-  if not (os.path.exists(vgfile_filename) 
-      and os.path.exists(vgfile_hostsfile)):
-    compile_testbed()
+    self.num_managers = math.ceil((self.config.testbed_size - 1) / 2)
+    self.num_workers = self.config.testbed_size - self.num_managers
 
-  subprocess.run(['vagrant', 'up'], cwd=testbed_build_dir)
+  def compile(self):
+    template_outfile = os.path.join(self.config.testbed_dir, 'Vagrantfile')
+    with open(template_outfile, 'w+') as file:
+      file.write(self.template.render(self.template_context))
+    
+    hosts_outfile = os.path.join(self.config.testbed_dir, 'hosts')
+    with open(hosts_outfile, 'w+') as file:
+      for i in range(self.config.testbed_size):
+        if i == 0: 
+          file.write('[managers]\n')
+        elif i == self.num_managers:
+          file.write('\n[workers]\n')
+        
+        ip_str = self.config.ip_mask_24 + str(i + 1 + self.config.ip_mask_8_offset)
+        file.write(ip_str + '\n')
+    
+    shutil.copyfile(
+      self.config.public_key_file, 
+      os.path.join(self.config.testbed_dir, 'server.pem.pub'))
+    shutil.copyfile(
+      self.config.private_key_file, 
+      os.path.join(self.config.testbed_dir, 'server.pem'))
 
-def halt_testbed():
-  config = read_config()
-  testbed_build_dir = testbed_dir(config)
-  vgfile_filename = os.path.join(testbed_build_dir, 'Vagrantfile')
-  vgfile_hostsfile = os.path.join(testbed_build_dir, 'hosts')
+  def ensure_compiled(self):
+    template_outfile = os.path.join(self.config.testbed_dir, 'Vagrantfile')
+    hosts_outfile = os.path.join(self.config.testbed_dir, 'hosts')
 
-  if not (os.path.exists(vgfile_filename) 
-      and os.path.exists(vgfile_hostsfile)):
-    compile_testbed()  
+    if not os.path.exists(template_outfile) \
+      or not os.path.exists(hosts_outfile):
+      self.compile()
+  
+  def up(self):
+    self.ensure_compiled()
+    subprocess.run(['vagrant', 'up'], cwd=self.config.testbed_dir)
 
-  testbed_size = int(config['Testbed']['TestbedSize'])
-  for i in range(testbed_size):
-    subprocess.run(['vagrant', 'halt', 'node-'+str(i+1)], cwd=testbed_build_dir)  
+  def halt(self):
+    self.ensure_compiled()
+    for i in range(self.config.testbed_size):
+      node_str = 'node-'+str(i+1)
+      subprocess.run(['vagrant', 'halt', node_str], cwd=self.config.testbed_dir)
 
-def destroy_testbed():
-  config = read_config()
-  testbed_build_dir = testbed_dir(config)
-  vgfile_filename = os.path.join(testbed_build_dir, 'Vagrantfile')
-  vgfile_hostsfile = os.path.join(testbed_build_dir, 'hosts')
+  def destroy(self):
+    self.ensure_compiled()
+    subprocess.run(['vagrant', 'destroy', '-f'], cwd=self.config.testbed_dir)  
 
-  if not (os.path.exists(vgfile_filename) 
-      and os.path.exists(vgfile_hostsfile)):
-    compile_testbed()  
 
-  subprocess.run(['killall', '-9', 'vagrant'], cwd=testbed_build_dir)  
-  subprocess.run(['killall', '-9', 'ruby'], cwd=testbed_build_dir)    
-  subprocess.run(['vagrant', 'destroy', '-f'], cwd=testbed_build_dir)  
+def usage():
+  print("Swarm CLI tool\n")
+  print("Use this tool to setup, test, work with, and deploy to a Docker Swarm cluster.\n")
+  print("REFERENCE")
+  print("python manage.py config - print out the complete configuration of the tool described by the swarm.ini file")
+  print("python manage.py testbed compile - compile the Vagrant testbed in order to launch some test nodes")
+  print("python manage.py testbed up - boot up all nodes in the Vagrant testbed")
+  print("python manage.py testbed halt - power down all nodes in the Vagrant testbed")
+  print("python manage.py testbed destroy - destroy all nodes in the Vagrant testbed")  
+  
 
 def command():
+  if len(sys.argv) == 1:
+    usage()
+    return
+
+  testbed = Testbed()
   service = sys.argv[1]
-  if service == 'testbed' and len(sys.argv) >= 3:
+  if service == 'config':
+    testbed.config.print_config()
+  elif service == 'testbed':
+    if len(sys.argv) == 2:
+      usage()
+      return
     option = sys.argv[2]
     if option == 'compile':
-      compile_testbed()
+      testbed.compile()
     elif option == 'up':
-      boot_up_testbed()
-    elif option == 'down':
-      halt_testbed()
+      testbed.up()
+    elif option == 'halt':
+      testbed.halt()
     elif option == 'destroy':
-      destroy_testbed()
+      testbed.halt()
     else:
-      print("using testbed service but did not provide valid option")
+      usage()
   else:
-    print("im walkin heeeerreee!!!")
+    usage()
 
 if __name__ == '__main__':
   command()
