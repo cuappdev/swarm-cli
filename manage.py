88293206eb11f18cee230a6879167ae63654f153
shivr2035 <shivr2035@gmail.com>
2018-05-02 18:22:43 -0400

    Migrate to using deployment bundles

--- manage.py
+++ manage.py
@@ -1,63 +1,81 @@
-import os, configparser, subprocess, shutil, sys, math, distutils.dir_util
-from jinja2 import Environment, FileSystemLoader, Template
-
-DRY_RUN = True
+import configparser
+import distutils.dir_util
+import jinja2
+import math
+import os 
+import shutil
+import subprocess
+import sys
+import texttable
 
 class Config:
 
-  def __init__(self):
+  def __init__(self, bundle_path=None):
     self.config = configparser.ConfigParser()
     self.config.read('swarm.ini')
 
     self.current_dir = os.path.dirname(os.path.abspath(__file__))
-    
     self.playbooks_dir = os.path.join(self.current_dir, 'playbooks')
     self.roles_dir = os.path.join(self.current_dir, 'roles')       
     self.ansible_cfg_dir = os.path.join(self.current_dir, 'ansible-cfg')
     self.templates_dir = os.path.join(self.current_dir, 'templates')
-    self.bld_dir = os.path.join(self.current_dir, '.bld')
+    self.build_dir = os.path.join(self.current_dir, '.bld')
+
+    if (bundle_path != None):
+      self.bundle_dir = os.path.abspath(bundle_path)
+    else:
+      self.bundle_dir = self.build_dir
+    self.compose_dir = os.path.join(self.bundle_dir, 'docker-compose')
+    self.hosts_file = os.path.join(self.bundle_dir, 'hosts')
+    self.public_key_file = os.path.join(self.bundle_dir, 'server.pem.pub')
+    self.private_key_file = os.path.join(self.bundle_dir, 'server.pem')
+    self.registry_crt_file = os.path.join(self.bundle_dir, 'domain.crt')
 
     self.using_testbed = self.config['Default']['UsingTestbed'] == 'true'
-    self.network_interface = self.config['Default']['NetworkInterface']
-    self.build_dir = os.path.join(self.bld_dir, self.config['Default']['BuildDirectory'])
-    self.compose_dir = os.path.join(self.current_dir, self.config['Default']['ComposeDirectory'])
-    self.hosts_file = os.path.join(self.current_dir, self.config['Default']['HostsFile'])
-    self.public_key_file = os.path.join(self.current_dir, self.config['Default']['PublicKeyFile'])
-    self.private_key_file = os.path.join(self.current_dir, self.config['Default']['PrivateKeyFile'])
-
     self.testbed_dir = os.path.join(self.build_dir, 'testbed')
     self.testbed_size = int(self.config['Testbed']['TestbedSize'])
     self.ip_mask_24 = self.config['Testbed']['IPMask24']
     self.ip_mask_8_offset = int(self.config['Testbed']['IPMask8Offset'])
-    self.testbed_network_interface = self.config['Testbed']['NetworkInterface']
-  
+
+  def make_table(self):
+    table = texttable.Texttable()
+    table.add_rows(
+      [
+        ['Parameter', 'Value'],
+        ['Current directory', self.current_dir],
+        ['Playbooks directory', self.playbooks_dir],
+        ['Roles directory', self.roles_dir],
+        ['Ansible config directory', self.ansible_cfg_dir],
+        ['Templates directory', self.templates_dir],
+        ['Build directory', self.build_dir],
+        ['', ''],
+        ['Bundle directory', self.bundle_dir],
+        ['Compose directory', self.compose_dir],
+        ['Hosts file', self.hosts_file],
+        ['SSH public key file', self.public_key_file],
+        ['SSH private key file', self.private_key_file],
+        ['Registry TLS Certificate', self.registry_crt_file],        
+        ['', ''],
+        ['Using testbed?', self.using_testbed],
+        ['Testbed directory', self.testbed_dir],
+        ['Testbed size', str(self.testbed_size)],
+        ['First 24 bits of testbed IPs', self.ip_mask_24],
+        ['Offet of last 8 bits of testbed IPs', str(self.ip_mask_8_offset)]
+      ]
+    )
+    return table
+
   def print_config(self):
-    print("Current directory = " + self.current_dir)
-    print("Playbooks directory = " + self.playbooks_dir)
-    print("Roles directory = " + self.roles_dir)
-    print("Ansible config directory = " + self.ansible_cfg_dir)
-    print("Templates directory = " + self.templates_dir)
-    print(".bld directroy = " + self.bld_dir)
-    print("Using testbed? " + str(self.using_testbed))
-    print("Network interface = " + self.network_interface)
-    print("Build directory = " + self.build_dir)
-    print("Compose directory = " + self.compose_dir)
-    print("Hosts file = " + self.hosts_file)
-    print("Public key file = " + self.public_key_file)
-    print("Private key file = " + self.private_key_file)
-    print("Testbed directory = " + self.testbed_dir)
-    print("Testbed size = " + str(self.testbed_size))
-    print("First 24 bits of testbed IPs = " + self.ip_mask_24)
-    print("Offset of last 8 bits of testbed IPs = " + str(self.ip_mask_8_offset))    
-    print("Testbed network interface = " + self.testbed_network_interface)    
+    table = self.make_table()
+    print(table.draw())     
 
 class Testbed:
 
-  def __init__(self):
-    self.config = Config()
+  def __init__(self, config):
+    self.config = config
     
-    self.env = Environment(
-      loader=FileSystemLoader(self.config.current_dir), 
+    self.env = jinja2.Environment(
+      loader=jinja2.FileSystemLoader(self.config.current_dir), 
       trim_blocks=True)
     self.template = self.env.get_template('templates/Vagrantfile.j2')
     self.template_context = {
@@ -98,14 +116,6 @@ class Testbed:
       self.config.private_key_file, 
       os.path.join(self.config.testbed_dir, 'server.pem'))
 
-  def ensure_compiled(self):
-    template_outfile = os.path.join(self.config.testbed_dir, 'Vagrantfile')
-    hosts_outfile = os.path.join(self.config.testbed_dir, 'hosts')
-
-    if not os.path.exists(template_outfile) \
-      or not os.path.exists(hosts_outfile):
-      self.compile()
-  
   def up(self):
     subprocess.run(['vagrant', 'up', '--parallel'], cwd=self.config.testbed_dir)
 
@@ -119,21 +129,16 @@ class Testbed:
 
 class Swarm:
 
-  def __init__(self):
-    self.testbed = Testbed()
-    self.config = self.testbed.config
+  def __init__(self, config):
+    self.config = config
     
     if not os.path.exists(self.config.build_dir):
       os.makedirs(self.config.build_dir)
     
     if self.config.using_testbed:
       self.hosts_buildfile = os.path.join(self.config.testbed_dir, 'hosts')
-      self.initial_ansible_config = 'vagrant.cfg'
-      self.network_interface = self.config.testbed_network_interface
     else:
       self.hosts_buildfile = self.config.hosts_file
-      self.initial_ansible_config = 'root.cfg'
-      self.network_interface = self.config.network_interface
 
 
   def compile(self):
@@ -143,8 +148,11 @@ class Swarm:
     shutil.copy(
       self.config.private_key_file, 
       os.path.join(self.config.build_dir, 'server.pem'))
-    os.chmod(os.path.join(self.config.build_dir, 'server.pem'), 0o400)
     
+    shutil.copy(
+      self.config.registry_crt_file, 
+      os.path.join(self.config.build_dir, 'domain.crt'))
+
     distutils.dir_util.copy_tree(
       self.config.roles_dir, 
       os.path.join(self.config.build_dir, 'roles'))
@@ -195,11 +203,11 @@ class Swarm:
       cwd=self.config.build_dir, 
       env=temp_env).wait()
     subprocess.Popen(
-      ['ansible-playbook', 'swarm-join.yml', '--extra-vars', 'swarm_iface=' + self.network_interface], 
+      ['ansible-playbook', 'swarm-join.yml'], 
       cwd=self.config.build_dir,
       env=temp_env).wait()
   
-  def upload(self):
+  def configure(self):
     temp_env = os.environ.copy()
     temp_env['ANSIBLE_CONFIG'] = 'appdev.cfg'
     subprocess.Popen(
@@ -207,7 +215,7 @@ class Swarm:
       cwd=self.config.build_dir, 
       env=temp_env).wait()
 
-  def down(self):
+  def clean(self):
     temp_env = os.environ.copy()
     temp_env['ANSIBLE_CONFIG'] = 'appdev.cfg'
     subprocess.Popen(
@@ -219,37 +227,50 @@ def usage():
   print("Swarm CLI tool\n")
   print("Use this tool to setup, test, work with, and deploy to a Docker Swarm cluster.\n")
   print("REFERENCE")
-  print("python manage.py config - print out the complete configuration of the tool described by the swarm.ini file")
+  print("python manage.py compile BUNDLE_DIR swarm - compile the swarm tool using the bundle at path BUNDLE_DIR")  
+  print("python manage.py compile BUNDLE_DIR testbed - compile the testbed using the bundle at path BUNDLE_DIR")  
+  print("python manage.py config - print out the complete configuration described by the swarm.ini file and the curent compilation")
   print("")
-  print("python manage.py testbed compile - compile the Vagrant testbed in order to launch some test nodes")
   print("python manage.py testbed up - boot up all nodes in the Vagrant testbed")
   print("python manage.py testbed halt - power down all nodes in the Vagrant testbed")
   print("python manage.py testbed destroy - destroy all nodes in the Vagrant testbed")  
   print("")
-  print("python manage.py swarm compile - compile all the deployment files for provisioning the target machines")  
   print("python manage.py swarm lockdown - lock down access to the target machines")  
   print("python manage.py swarm join - join the target machines into a Docker Swarm")
-  print("python manage.py swarm upload - upload the Docker Compose file to the first manager of the Swarm")  
-  print("python manage.py swarm down - remove Docker Stack, containers, and images from all machines")    
+  print("python manage.py swarm configure - upload the Docker Compose file to the first manager of the Swarm")  
+  print("python manage.py swarm clean - remove Docker Stack, containers, and images from all machines")    
 
 def command():
   if len(sys.argv) == 1:
     usage()
     return
-  swarm = Swarm()
-  testbed = swarm.testbed
-  config = swarm.config
+  config = Config()
   service = sys.argv[1]
-  if service == 'config':
+  testbed = Testbed(config)
+  swarm = Swarm(config)
+  if service == 'compile':
+    if len(sys.argv) != 4:
+      usage()
+      return
+    config = Config(sys.argv[2])
+    testbed = Testbed(config)
+    swarm = Swarm(config)
+    option = sys.argv[3]
+    if option == 'swarm':
+      swarm.compile()
+    elif option == 'testbed':
+      testbed.compile()
+    else:
+      usage()
+      return
+  elif service == 'config':
     config.print_config()
   elif service == 'testbed':
     if len(sys.argv) == 2:
       usage()
       return
     option = sys.argv[2]
-    if option == 'compile':
-      testbed.compile()
-    elif option == 'up':
+    if option == 'up':
       testbed.up()
     elif option == 'halt':
       testbed.halt()
@@ -262,16 +283,14 @@ def command():
       usage()
       return
     option = sys.argv[2]
-    if option == 'compile':
-      swarm.compile()
-    elif option == 'lockdown':
+    if option == 'lockdown':
       swarm.lockdown()
     elif option == 'join':
       swarm.join()
-    elif option == 'upload':
-      swarm.upload()
-    elif option == 'down':
-      swarm.down()
+    elif option == 'configure':
+      swarm.configure()
+    elif option == 'clean':
+      swarm.clean()
     else:
       usage()
   else:
